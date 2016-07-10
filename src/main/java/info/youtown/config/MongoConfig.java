03425c30f183238e5e8a5646fef689966f709c45
lemonjp <luckylurgee@gmail.com>
2016-07-10 10:54:49 +0000

    mod for mongo config.

--- src/main/java/info/youtown/config/MongoConfig.java
+++ src/main/java/info/youtown/config/MongoConfig.java
@@ -3,28 +3,31 @@ package info.youtown;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.beans.factory.annotation.Value;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 
 @Configuration
-//@EnableMongoRepositories
 public class MongoConfig extends AbstractMongoConfiguration {
 
-    @Value("${spring.data.mongodb.database}")
-    private String databasename;
+    @Value("${spring.data.mongodb.host}")
+    private String mongoHost;
 
-    @Override
-    protected String getDatabaseName() {
-        return databasename;
-        //return "yourdb2";
-    }
+    @Value("${spring.data.mongodb.port}")
+    private String mongoPort;
+
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDB;
 
     @Override
     @Bean
     public Mongo mongo() throws Exception {
-        return new MongoClient("127.0.0.1");
+        return new MongoClient(mongoHost + ":" + mongoPort);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return mongoDB;
     }
 }
