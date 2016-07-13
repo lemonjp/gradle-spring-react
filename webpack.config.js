7c9561387cd8c25b319db1c084c0b2e64f9e919b
lemonjp <luckylurgee@gmail.com>
2016-07-13 11:27:41 +0000

    mod for webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,15 @@
 var path = require('path');
 var ROOT = path.resolve(__dirname, 'src/main/resources/static');
-// var DEST = path.resolve(__dirname, 'src/main/resources/static/dist');
 var DEST = path.resolve(__dirname, 'src/main/resources/static/dist');
 
 module.exports = {
-    //entry: './app.js',
     devtool: 'sourcemaps',
     entry: ROOT + '/app.js',
-    /*
-    entry: {
-        app: ROOT + '/app.js',
-    },
-    */
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
     cache: true,
     debug: true,
-    /*
-    output: {
-        path: __dirname,
-        filename: './built/bundle.js'
-    },
-    */
     output: {
         path: DEST,
         filename: 'bundle.js',
@@ -31,10 +18,14 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                loader: 'babel-loader'
+                test: path.join(__dirname, '/\.js[x]?$/'),
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                  presets: [ 'es2015', 'react' ]
+                }
             }
         ]
+
     }
 };
